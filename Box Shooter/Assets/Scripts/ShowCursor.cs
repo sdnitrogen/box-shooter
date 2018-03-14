b9a76a44d493a0377eb88dad722bde184113d20a
AssassiNCrizR <skd.bwn96@gmail.com>
2018-03-14 23:10:37 +0530

    first commit

---
+++ Box Shooter/Assets/Scripts/ShowCursor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LockCursor (false);
+	}
+	
+private void LockCursor(bool isLocked)
+	{
+		if (isLocked) 
+		{
+			// make the mouse pointer invisible
+			Cursor.visible = false;
+
+			// lock the mouse pointer within the game area
+			Cursor.lockState = CursorLockMode.Locked;
+		} else {
+			// make the mouse pointer visible
+			Cursor.visible = true;
+
+			// unlock the mouse pointer so player can click on other windows
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+	
+}
+
+
